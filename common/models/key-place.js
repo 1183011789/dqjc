436ffe3a442f4d161917894566a8712a0f7056ca
jiawenlong <wangjianing@wangjianingdeiMac.local>
2017-08-16 23:12:50 -0700

    ”initnewimg”

--- common/models/key-place.js
+++ common/models/key-place.js
@@ -115,4 +115,52 @@ console.log(one);
         }
     ]
     });
+    Keyplace.uploadPictrue = function (ctx,options,cb){
+        options = options || {};
+        //if(!options) options = {};
+        ctx.req.params.container = 'common1'; // "common" 为之前数据源中root 参数 /server/storage 目录下的文件夹“common” 需要自己创建好
+        Keyplace.app.models.Container.upload(ctx.req, ctx.result,
+        options, function(err, fileObj) {
+          if (err) {
+            return cb(null, {
+              status: 'failed',
+              message: err.message,
+            });
+          } else {
+            // The 'file'below should be the same as field name in the form
+            var fileInfoArr = fileObj.files.file;
+            var objs = [];
+            fileInfoArr.forEach(function(item) {
+              objs.push({
+                name: item.name,
+                type: item.type,
+                url: CONTAINER_URL + item.container +
+                      '/download/' + item.name,
+              });
+            });
+            Keyplace.create(objs, function(err, instances) {
+              if (err) {
+                return cb(null, {
+                  message: err.message,
+                });
+              } else {
+                cb(null, instances);
+              }
+            });
+          }
+        });
+    };
+    Keyplace.remoteMethod(
+      'upload', {
+        description: 'Upload a file or more files',
+        accepts: [
+          {arg: 'ctx', type: 'object', http: {source: 'context'}},
+          {arg: 'options', type: 'object', http: {source: 'query'}},
+        ],
+        returns: {
+          arg: 'fileObject', type: 'object', root: true,
+        },
+        http: {verb: 'post'},
+      }
+    );
 };
