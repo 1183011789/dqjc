78824e8db2431beadaa0e63f5b8ed0ec79e1c00b
jiawenlong <wangjianing@wangjianingdeiMac.local>
2017-08-18 01:35:21 -0700

    new-develop

--- common/models/rode.js
+++ common/models/rode.js
@@ -20,6 +20,11 @@ module.exports = function(Rode) {
                       console.log(err);
                       }
                   });
+        // var conn = this.app.datasources['bjhlb_mysql'].connector;
+        // var sql ='select * from Rode where rodename like '%'+rodename+'%' or classification = '+classification+'';
+        // conn.executeSQL(sql, [], {}, function(err, back) {
+        //   callback(err, back);
+        // });
 
         }
         Rode.remoteMethod(
@@ -55,7 +60,7 @@ module.exports = function(Rode) {
       //   return false;
       //   }
       //   }
-      
+
 
         var pageCount=0;//总页数
         Rode.count( {}, function (err, result) {
@@ -109,5 +114,25 @@ module.exports = function(Rode) {
               }
           ]
           });
+
+
+
+
+  Rode.fetchRode = function fetchRode(callback) {
+    //console.log(this.app.datasources['bjhlb_mysql']);
+    var conn = this.app.datasources['bjhlb_mysql'].connector;
+    var sql ='SELECT DISTINCT  distinct(classification) from Rode';
+    conn.executeSQL(sql, [], {}, function(err, back) {
+      callback(err, back);
+    });
+  };
+  Rode.remoteMethod('fetchRode', {
+    accepts: [ ],
+    returns: { arg: 'rodes', type: ['object'] },
+    http: {verb: 'get'},
+  });
+
+
+
 
 };
