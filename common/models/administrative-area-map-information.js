d0e1da2602d86f29adbe5f58a3cc2505d3f20cdb
jiawenlong <wangjianing@wangjianingdeiMac.local>
2017-08-25 00:14:26 -0700

    new

--- common/models/administrative-area-map-information.js
+++ common/models/administrative-area-map-information.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = function(Administrativeareamapinformation) {
-
+  // Administrativeareamapinformation.RemoveAdministrativeareamapinformationRank = function fetchAdministrativeareamapinformation(callback) {
+  //   //console.log(this.app.datasources['bjhlb_mysql']);
+  //   var conn = this.app.datasources['bjhlb_mysql'].connector;
+  //   var sql ='';
+  //   conn.executeSQL(sql, [], {}, function(err, back) {
+  //     callback(err, back);
+  //   });
+  // };
+  // Administrativeareamapinformation.remoteMethod('RemoveStationRank', {
+  //   accepts: [ ],
+  //   returns: { arg: 'rodes', type: ['object'] },
+  //   http: {verb: 'get'},
+  // });
 };
